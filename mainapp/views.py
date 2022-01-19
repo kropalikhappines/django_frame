0e8ea094dc03906adaa2e1d7649061c96f9ade4d
kropalikhappines <arsik30394@gmail.com>
2022-01-19 14:39:07 +0300

    Lesson 7. Code style fix

--- mainapp/views.py
+++ mainapp/views.py
@@ -39,7 +39,7 @@ def products(request, pk=None):
     title = "продукты"
     links_menu = ProductCategory.objects.all()
     basket = get_basket(request.user)
-    
+
     if pk is not None:
         if pk == 0:
             products = Product.objects.all().order_by("price")
