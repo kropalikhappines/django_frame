c2ca4ab033da2ac040ffcba5e0240607e540d281
kropalikhappines <arsik30394@gmail.com>
2022-01-19 09:27:38 +0300

    lesson_7. Adminapp.

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     "mainapp",
     "authapp",
     "basketapp",
+    "adminapp",
 ]
 
 # Auth model
--- geekshop/urls.py
+++ geekshop/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path("contact/", mainapp.contact, name="contact"),
     path("auth/", include("authapp.urls", namespace="auth")),
     path("basket/", include("basketapp.urls", namespace="basket")),
+    path("admin/", include("adminapp.urls", namespace="admin")),
 ]
 
 
