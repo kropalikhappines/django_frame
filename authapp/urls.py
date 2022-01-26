03edce32a422f3e5db7112aa975f959682a993cc
kropalikhappines <arsik30394@gmail.com>
2022-01-26 09:54:32 +0300

    lesson_9. Send email to file.

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,13 +1,14 @@
 from django.urls import re_path
 
 import authapp.views as authapp
-from authapp.apps import AuthappConfig
+from authapp.apps import AuthnappConfig
 
-app_name = AuthappConfig.name
+app_name = AuthnappConfig.name
 
 urlpatterns = [
     re_path(r"^login/$", authapp.login, name="login"),
     re_path(r"^logout/$", authapp.logout, name="logout"),
     re_path(r"^register/$", authapp.register, name="register"),
     re_path(r"^edit/$", authapp.edit, name="edit"),
+    re_path(r"^verify/(?P<email>.+)/(?P<activation_key>\w+)/$", authapp.verify, name="verify"),
 ]
