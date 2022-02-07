6b869267b9dfa1f739ad150f3610cec4012f9b08
kropalikhappines <arsik30394@gmail.com>
2022-02-07 11:41:59 +0300

    Lesson 11. Code style fix

--- authapp/models.py
+++ authapp/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
 from django.utils.timezone import now
 
 
@@ -46,4 +45,3 @@ class ShopUserProfile(models.Model):
     @receiver(post_save, sender=ShopUser)
     def save_user_profile(sender, instance, **kwargs):
         instance.shopuserprofile.save()
-
